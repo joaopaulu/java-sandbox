53487194286480ebcf944f941018f43f3a00e786
jp <jptick@gmail.com>
2020-11-23 07:10:00 -0300

    Functions lambdas

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -12,22 +12,20 @@ public class OrdenaStrings {
         
         Comparator<String> comparador = new ComparadorPorTamanho();
        
-        palavras.sort(comparador);
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
         System.out.println(palavras);
-        
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
+
+        palavras.forEach((s) -> System.out.println(s));
+
     }
 }
 
 class ImprimeNaLinha implements Consumer<String>{
-
 	@Override
 	public void accept(String s) {
 		System.out.println(s);
-		
 	}
-	
 }
 
 class ComparadorPorTamanho implements Comparator<String>{
