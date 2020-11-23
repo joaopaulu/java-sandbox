ff9a1a65fa0139863e6403c866c874b8206e3db1
jp <jptick@gmail.com>
2020-11-23 11:20:16 -0300

    Update lambda function

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -8,35 +8,26 @@ public class OrdenaStrings {
         List<String> palavras = new ArrayList<String>();
         palavras.add("alura online");
         palavras.add("editora casa do codigo");
-        palavras.add("caelum");
-        
-        Comparator<String> comparador = new ComparadorPorTamanho();
-       
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+				palavras.add("caelum");
+				
+				palavras.sort((s1, s2) -> {
+					if(s1.length() < s2.length())
+						return -1;
+					if(s1.length() > s2.length())
+						return 1;
+					return 0;	
+				});
 
-        System.out.println(palavras);
+				palavras.sort(Comparator.comparing(s -> s.length()));
+				palavras.sort(Comparator.comparing(String::length));
+
+				System.out.println(palavras);
+				
+				Consumer<String> impressor = System.out::println;
+				palavras.forEach(impressor);
 
         palavras.forEach((s) -> System.out.println(s));
 
     }
-}
-
-class ImprimeNaLinha implements Consumer<String>{
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
-
-class ComparadorPorTamanho implements Comparator<String>{
-	
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length())
-			return -1;
-		if(s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
 }
 
