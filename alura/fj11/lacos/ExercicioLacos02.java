72a582cf8f8dcb47ae91c076840b663bb50b20e6
joaopaulu <jptick@gmail.com>
2020-12-01 08:49:07 -0300

    Exercicios de Laços

---
+++ alura/fj11/lacos/ExercicioLacos02.java
@@ -0,0 +1,11 @@
+package lacos;
+
+public class ExercicioLacos02 {
+  public static void main(String[] args) {
+    //2 - Imprima a soma de 1 até 1000   
+    for(int i = 1; i <= 1000; i++){
+      int result = i + i;      
+      System.out.println("A soma de "+ i +" + "+ i + " é = " + result);
+    }
+  }
+}
