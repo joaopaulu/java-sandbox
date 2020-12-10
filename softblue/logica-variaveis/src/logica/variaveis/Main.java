a0c50b1d8fec5e97ffb743e5f264462cd7e67f98
joaopaulu <jptick@gmail.com>
2020-12-09 21:52:50 -0300

    Init softblue course

---
+++ softblue/logica-variaveis/src/logica/variaveis/Main.java
@@ -0,0 +1,18 @@
+package logica.variaveis;
+
+public class Main {
+    public static void main(String[] args) {
+        int idade;
+        idade = 20;
+        double nota = 8.6;
+        boolean ligado = true;
+        boolean ativo = false;
+        String nome = "JP";
+
+        System.out.println(nota);
+
+        System.out.println(nome + "->" + nota);
+
+
+    }
+}
