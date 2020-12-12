01146635a450354391b8e1d37510558430a9eeb7
joaopaulu <jptick@gmail.com>
2020-12-12 14:47:14 -0300

    Test variables

---
+++ softblue/logica-variavel/src/main/java/logica/variavel/Main.java
@@ -0,0 +1,17 @@
+package logica.variavel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int idade;
+		idade = 20;
+		double nota = 8.5;
+		boolean ligado = true;
+		boolean ativo = false;
+		String nome = "JP";
+		
+		System.out.println(nome + " -> " + nota);
+
+	}
+
+}
