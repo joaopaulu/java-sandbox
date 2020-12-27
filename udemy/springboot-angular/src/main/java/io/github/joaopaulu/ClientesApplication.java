1ab7b3705bced0e6416eb2f76cc1f972a3754a82
joaopaulu <jptick@gmail.com>
2020-12-27 09:57:05 -0300

    Setting Lombok and Create Entity Cliente

--- udemy/springboot-angular/src/main/java/io/github/joaopaulu/ClientesApplication.java
+++ udemy/springboot-angular/src/main/java/io/github/joaopaulu/ClientesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VendasApplication {
+public class ClientesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VendasApplication.class, args);
+        SpringApplication.run(ClientesApplication.class, args);
     }
 }
