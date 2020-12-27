cd3f30fa8211577c258c0b5014cadcb80ee96b88
joaopaulu <jptick@gmail.com>
2020-12-27 16:20:43 -0300

    Updade Endpoints CRUD Clientes

--- udemy/springboot-angular/src/main/java/io/github/joaopaulu/model/Cliente.java
+++ udemy/springboot-angular/src/main/java/io/github/joaopaulu/model/Cliente.java
@@ -1,26 +1,36 @@
 package io.github.joaopaulu.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable=false, length=150)
+    @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
-    @Column(nullable=false, length=11)
+    @Column(nullable = false, length = 11)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
-    @Column(name = "data_cadastro", updatable=false)
-    @JsonFormat(pattern = "dd/MM/yyy")
+    @Column(name = "data_cadastro", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
     @PrePersist
--- udemy/springboot-angular/src/main/java/io/github/joaopaulu/resource/ClienteResource.java
+++ udemy/springboot-angular/src/main/java/io/github/joaopaulu/resource/ClienteResource.java
@@ -7,54 +7,61 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/clientes")
 public class ClienteResource {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+    private final ClienteRepository repository;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente){
-        return clienteRepository.save(cliente);
+    @Autowired
+    public ClienteResource(ClienteRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping
-    public List<Cliente> listar(){
-        return clienteRepository.findAll();
+    public List<Cliente> obterTodos(){
+        return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar( @RequestBody @Valid Cliente cliente ){
+        return repository.save(cliente);
     }
 
     @GetMapping("{id}")
-    public Cliente buscarPorId(@PathVariable Integer id){
-        return clienteRepository
+    public Cliente acharPorId( @PathVariable Integer id ){
+        return repository
                 .findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletar(@PathVariable Integer id){
-        clienteRepository
-            .findById(id)
-            .map(cliente ->{
-                clienteRepository.delete(cliente);
-                return Void.TYPE;
-            })
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public void deletar( @PathVariable Integer id ){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado){
-        clienteRepository
-            .findById(id)
-            .map(cliente ->{
-                clienteAtualizado.setId(cliente.getId());
-                return clienteRepository.save(clienteAtualizado);
-            })
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public void atualizar( @PathVariable Integer id,
+                           @RequestBody @Valid Cliente clienteAtualizado ) {
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    cliente.setNome(clienteAtualizado.getNome());
+                    cliente.setCpf(clienteAtualizado.getCpf());
+                    return repository.save(cliente);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 }
