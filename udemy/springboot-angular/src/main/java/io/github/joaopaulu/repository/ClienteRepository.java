f4b2df1b33c8c1c28bc6b81c5f6197626066a48b
João Paulo <jptick@gmail.com>
2021-01-10 13:30:54 -0300

    Change Folders Structure

--- udemy/springboot-angular/src/main/java/io/github/joaopaulu/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.joaopaulu.repository;
-
-import io.github.joaopaulu.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
