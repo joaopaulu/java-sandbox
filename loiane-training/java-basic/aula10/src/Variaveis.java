5a45bb9b350ff71093e74e793c09d3a60e86af83
João Paulo <jptick@gmail.com>
2021-04-15 21:39:16 -0300

    Test Variables

---
+++ loiane-training/java-basic/aula10/src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade;
+        String nome;
+        String nomeCachorro;
+
+        idade = 25;
+
+        System.out.println("Idade = " + idade);
+
+    }
+}
