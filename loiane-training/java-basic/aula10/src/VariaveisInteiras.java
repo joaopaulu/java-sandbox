9803dac605382d20b4e0486190f1bad8d52104a5
João Paulo <jptick@gmail.com>
2021-04-16 18:14:47 -0300

    Update Test Variables

---
+++ loiane-training/java-basic/aula10/src/VariaveisInteiras.java
@@ -0,0 +1,10 @@
+public class VariaveisInteiras {
+    public static void main(String[] args) {
+        byte idade01 = 20;
+        short idade02 = 21;
+
+        System.out.println(" valor variável idade01 = " + idade01);
+        System.out.println(" valor variável idade02 = " + idade02);
+
+    }
+}
