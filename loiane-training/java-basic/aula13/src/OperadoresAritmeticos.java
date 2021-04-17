a7c3f5524e422725359f86cf5847daabd083388c
João Paulo <jptick@gmail.com>
2021-04-17 10:40:41 -0300

    Using Arithmetics Operators

---
+++ loiane-training/java-basic/aula13/src/OperadoresAritmeticos.java
@@ -0,0 +1,12 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 2;
+        System.out.println(resultado);
+    }
+}
