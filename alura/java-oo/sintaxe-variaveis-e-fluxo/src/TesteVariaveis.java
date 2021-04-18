1d9a6eb5c8a0c25890d10a7ef6ebf546418385e3
João Paulo <jptick@gmail.com>
2021-04-18 09:59:15 -0300

    Try Variables Types

---
+++ alura/java-oo/sintaxe-variaveis-e-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,12 @@
+public class TesteVariaveis {
+    public static void main(String[] args) {
+        System.out.println("ola novo teste");
+
+        int idade = 36;
+        double salario = 1250.70;
+        System.out.println("Meu salario é " + salario);
+
+        double divisao = 3.14 / 4;
+        System.out.println(divisao);
+    }
+}
