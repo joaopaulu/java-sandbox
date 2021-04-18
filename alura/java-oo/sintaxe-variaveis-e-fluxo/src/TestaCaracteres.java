b89cff6b4a214a6775d9d05737019d2667b15072
João Paulo <jptick@gmail.com>
2021-04-18 10:38:39 -0300

    Work With Chars

---
+++ alura/java-oo/sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,12 @@
+public class TestaCaracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 66;
+        System.out.println(valor);
+
+        String palavra = "alura cursos online de tecnologia";
+        System.out.println(palavra);
+    }
+}
