9cad7f1341c8594eb33cca06ab65aae8c81f362c
João Paulo <jptick@gmail.com>
2021-04-25 11:21:36 -0300

    Numeric Types Java

--- algaworks/CursoLogicaCapituloVariaves/src/TiposNumericos.java
+++ algaworks/CursoLogicaCapituloVariaves/src/TiposNumericos.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class TiposNumericos {
+public class TiposNumericos {
+    public static void main(String[] args) {
+        Byte numeroByte = 1; // -128 até 127
+
+        Short numeroShort = 1; // -32768 até 32767
+
+        Integer numeroInteger = 1; // -2147483648 até 2147483647
+
+        Long numeroLong = 1L; // -9,223372037X10² até 9,223372037X10²
+
+        Float numeroFloat = 1.0f;
+
+        Double numeroDouble = 1.0;
+    }
 }
